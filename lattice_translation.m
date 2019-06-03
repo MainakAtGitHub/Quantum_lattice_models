a9672cd441e216e1d0f09a66efdd85af2c1d32af
kreisel <kreisel@4651d5b5-20ad-48e2-91c6-fc46191f00f4>
2019-06-03 17:51:15 +0000

    support for non-rectangular system sizes
    git-svn-id: svn+ssh://itp.uni-frankfurt.de/home/kreisel/svn/susc/BdG@84 4651d5b5-20ad-48e2-91c6-fc46191f00f4

--- lattice_translation.m
+++ lattice_translation.m
@@ -7,50 +7,56 @@ function t = lattice_translation(N, TBparameters, latticeVectors)
 
 
 % to be done: generalize to systems that are not NxN but N(1)xN(2), i.e.
-% rectangular
-
+% rectangular by default
+if numel(N)==1
+     N=[N N];
+end
 nOrbitals = size(TBparameters,1);
-maxHop = max(max(abs(latticeVectors)));
+maxHop = max(abs(latticeVectors(:,1:2)),[],1);
 minSystemSize = 2*maxHop + 1;
-if N < minSystemSize
+% no special treatment for hoppings that extend differently in x and y directions
+% just make the system big enougth in both directions
+if sum(N < minSystemSize) >0
     error(['System size must be greater than or equal',num2str(minSystemSize)]);
 end
-t = zeros(N^2*nOrbitals,N^2*nOrbitals);
-for ix = 1:N
-    for iy = 1:N
-        for jx = 1:N
-            for jy = 1:N
+% to be done 1)flag for boundary conditions
+% 2) sparse matrix algebra switch
+t = zeros(N(1)*N(2)*nOrbitals,N(1)*N(2)*nOrbitals);
+for ix = 1:N(1)
+    for iy = 1:N(2)
+        for jx = 1:N(1)
+            for jy = 1:N(2)
                 diffX = ix - jx;
                 diffY = iy - jy;
-                if (abs(diffX) <  N - maxHop) && (abs(diffY) <  N - maxHop)
+                if (abs(diffX) <  N(1) - maxHop(1)) && (abs(diffY) <  N(2) - maxHop(2))
                     % direct hop
                     ind = find((latticeVectors(:,1) == diffX) & (latticeVectors(:,2) == diffY));
                     if ~isempty(ind)
-                        iRange = (((ix-1)*N + iy - 1)*nOrbitals + 1):((ix-1)*N + iy)*nOrbitals;
-                        jRange = (((jx-1)*N + jy - 1)*nOrbitals + 1):((jx-1)*N + jy)*nOrbitals;
+                        iRange = (((ix-1)*N(2) + iy - 1)*nOrbitals + 1):((ix-1)*N(2) + iy)*nOrbitals;
+                        jRange = (((jx-1)*N(2) + jy - 1)*nOrbitals + 1):((jx-1)*N(2) + jy)*nOrbitals;
                         t(iRange, jRange) = TBparameters(:,:,ind);
                     end
-                elseif (abs(diffX) <  N - maxHop) && (abs(diffY) >=  N - maxHop)
+                elseif (abs(diffX) <  N(1) - maxHop(1)) && (abs(diffY) >=  N(2) - maxHop(2))
                     % periodic hop
-                    ind = find((latticeVectors(:,1) == diffX) & (latticeVectors(:,2) == diffY - sign(diffY)*N));
+                    ind = find((latticeVectors(:,1) == diffX) & (latticeVectors(:,2) == diffY - sign(diffY)*N(2)));
                     if ~isempty(ind)
-                        iRange = (((ix-1)*N + iy - 1)*nOrbitals + 1):((ix-1)*N + iy)*nOrbitals;
-                        jRange = (((jx-1)*N + jy - 1)*nOrbitals + 1):((jx-1)*N + jy)*nOrbitals;
+                        iRange = (((ix-1)*N(2) + iy - 1)*nOrbitals + 1):((ix-1)*N(2) + iy)*nOrbitals;
+                        jRange = (((jx-1)*N(2) + jy - 1)*nOrbitals + 1):((jx-1)*N(2) + jy)*nOrbitals;
                         t(iRange, jRange) = TBparameters(:,:,ind);
                     end
-                elseif (abs(diffY) <  N - maxHop) && (abs(diffX) >=  N - maxHop)
+                elseif (abs(diffY) <  N(2) - maxHop(2)) && (abs(diffX) >=  N(1) - maxHop(1))
                     % periodic hop
-                    ind = find((latticeVectors(:,1) == diffX - sign(diffX)*N) & (latticeVectors(:,2) == diffY));
+                    ind = find((latticeVectors(:,1) == diffX - sign(diffX)*N(1)) & (latticeVectors(:,2) == diffY));
                     if ~isempty(ind)
-                        iRange = (((ix-1)*N + iy - 1)*nOrbitals + 1):((ix-1)*N + iy)*nOrbitals;
-                        jRange = (((jx-1)*N + jy - 1)*nOrbitals + 1):((jx-1)*N + jy)*nOrbitals;
+                        iRange = (((ix-1)*N(2) + iy - 1)*nOrbitals + 1):((ix-1)*N(2) + iy)*nOrbitals;
+                        jRange = (((jx-1)*N(2) + jy - 1)*nOrbitals + 1):((jx-1)*N(2) + jy)*nOrbitals;
                         t(iRange, jRange) = TBparameters(:,:,ind);
                     end
                 else
-                   ind = find((latticeVectors(:,1) == diffX - sign(diffX)*N) & (latticeVectors(:,2) == diffY - sign(diffY)*N)); 
+                   ind = find((latticeVectors(:,1) == diffX - sign(diffX)*N(1)) & (latticeVectors(:,2) == diffY - sign(diffY)*N(2))); 
                    if ~isempty(ind)
-                        iRange = (((ix-1)*N + iy - 1)*nOrbitals + 1):((ix-1)*N + iy)*nOrbitals;
-                        jRange = (((jx-1)*N + jy - 1)*nOrbitals + 1):((jx-1)*N + jy)*nOrbitals;
+                        iRange = (((ix-1)*N(2) + iy - 1)*nOrbitals + 1):((ix-1)*N(2) + iy)*nOrbitals;
+                        jRange = (((jx-1)*N(2) + jy - 1)*nOrbitals + 1):((jx-1)*N(2)+ jy)*nOrbitals;
                         t(iRange, jRange) = TBparameters(:,:,ind);
                     end
                 end
