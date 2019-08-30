57077de5a4936def0419e5596fce3f26ec48d9e1
kreisel <kreisel@4651d5b5-20ad-48e2-91c6-fc46191f00f4>
2019-08-30 07:37:44 +0000

    updated snipplet for band calculation
    git-svn-id: svn+ssh://itp.uni-frankfurt.de/home/kreisel/svn/susc/BdG@106 4651d5b5-20ad-48e2-91c6-fc46191f00f4

--- bands_klist.m
+++ bands_klist.m
@@ -1,55 +1,56 @@
 function [kSpaceEigenValuesNormal,kSpaceEigenVectorsNormal]=bands_klist(inputfile,klistfile)
 
-% Modified homogeneous_dos.m
-% takes \Delta_ij as input and constructs \Delta_i0.
+% for given inputfile (*.txt) and a file containing k-points, calculate the eigenvalues and eigenvectors
+% of the band structure
 
-if nargin <1
-    % load relevant files
-    TB_file='TB_hamiltonian_FeSe_2D.mat'
-    BdGfileName='BdG_homogeneous_FeSe_Milan_GammaCut_2_N_9(1).mat'
-    Gamma_file='Gamma_FeSe_Milan_GammaCut_2.mat'
-    M = input('Enter no of k-points   ');% no of K points in x
-    ita = input('Enter ita   '); % broadening
-    firstEnergy = input('Enter starting energy   ');
-    lastEnergy = input('Enter last energy   ');
-    nEnergyPoints = input('Enter no of energy points   ');
-else
+% by default set the chemical potential to zero
+ mu=0;
     % load the input file to set the variables, gave up the old .mat file
     % format
      read_input_file=inputfile;
      read_input;
      read_input_file
-end;
 
+
+% load the hoppings from the file
 load(TB_file,'-mat');
 nOrbitals = size(TBparameters,1);
-    mu=0;
 
-
+% number of hoppings in band structure
 nUnitCells = size(latticeVector,1);
+
+% allow to set the chemical potential
 TBparameters(:,:,(latticeVector(:,1)==0) & (latticeVector(:,2)==0)) = ...
 TBparameters(:,:,(latticeVector(:,1)==0) & (latticeVector(:,2)==0)) - mu*eye(nOrbitals);
-% fix for the lattice vectors
+
+% fix for the lattice vectors (no 3rd component allowed)
 latticeVector=latticeVector(:,1:2);
 
 % read in k values
 kin=load(klistfile);
 szk=size(kin);
+% initialize the result variables
 kSpaceEigenValuesNormal = zeros(szk(1), nOrbitals);
 kSpaceEigenVectorsNormal = zeros(szk(1), nOrbitals, nOrbitals);
+
+%iterate over the momentum vectors
 for iKx = 1:szk(1)
+	    % set the momentum vector (in 2D)
             k = [kin(iKx,1) kin(iKx,2)];
-            % diagonalizing for normal state DOS
+	    % initialize the Hamiltonian to zeros
             kSpaceHopping = zeros(nOrbitals,nOrbitals);
+	    % add all terms coming from each hopping
             for iUnitCell = 1:nUnitCells
                 iLatticeVector = latticeVector(iUnitCell,:);
+		% do (manually) the Fourier transform and add to the Hamiltonian
                 kSpaceHopping = kSpaceHopping + TBparameters(:,:,iUnitCell)*exp(1i*(iLatticeVector*k'));
-            end                        
+            end
+	    % diagonalize the Hamiltonian
             [eigVectorNormal, eigValueNormal] = eig(kSpaceHopping);
             [eigValueKNormal, sortingIndexNormal] = sort(real(diag(eigValueNormal)));
+	    % sort the eigenvalues and eigenvectors
             eigVectorKNormal = (eigVectorNormal(:,sortingIndexNormal))';
+	    % put them into the restult array
             kSpaceEigenValuesNormal(iKx, :) = eigValueKNormal;
             kSpaceEigenVectorsNormal(iKx, :,:) =  eigVectorKNormal;
 end
-
-
